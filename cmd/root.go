a4fb3da2465423be84780d1e4a33596078a54ca8
celaltas <celal.tas123@gmail.com>
2025-06-12 12:47:40 +0300

    --help added

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,3 @@
-/*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
@@ -10,25 +6,37 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	showCol1        bool
+	showCol2        bool
+	showCol3        bool
+	caseInsensitive bool
+	delimiter       string
+)
 
-
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "gocomm",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Use:   "gocomm [OPTION]... FILE1 FILE2",
+	Short: "Compare sorted files FILE1 and FILE2 line by line.",
+	Long: `Compare sorted files FILE1 and FILE2 line by line.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+With no options, produce three-column output:
+  column 1: lines only in FILE1
+  column 2: lines only in FILE2
+  column 3: lines in both FILE1 and FILE2
+
+Options:
+  -1                      suppress column 1 (lines unique to FILE1)
+  -2                      suppress column 2 (lines unique to FILE2)
+  -3                      suppress column 3 (lines that appear in both files)
+  -i, --insensitive       case-insensitive comparison
+  -d, --output-delimiter  separate columns with STR
+
+Examples:
+  gocomm file1.txt file2.txt
+  gocomm -12 file1.txt file2.txt
+  gocomm -i -d "|" file1.txt file2.txt`,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -37,15 +45,9 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gocomm.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().BoolVarP(&showCol1, "suppress1", "1", false, "suppress column 1 (lines unique to FILE1)")
+	rootCmd.Flags().BoolVarP(&showCol2, "suppress2", "2", false, "suppress column 2 (lines unique to FILE2)")
+	rootCmd.Flags().BoolVarP(&showCol3, "suppress3", "3", false, "suppress column 3 (lines that appear in both files)")
+	rootCmd.Flags().BoolVarP(&caseInsensitive, "insensitive", "i", false, "case-insensitive comparison")
+	rootCmd.Flags().StringVarP(&delimiter, "output-delimiter", "d", "\t", "separate columns with STR")
 }
-
-
